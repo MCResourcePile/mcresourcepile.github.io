dd17ba485c6845f2514f7d46b01ebd04d0119c1a
Matic0B <Matic0B@users.noreply.github.com>
2017-01-11 18:49:56 +0100

    Fix revision name color

--- assets/js/names.js
+++ assets/js/names.js
@@ -1,7 +1,7 @@
  window.onload = function() {
    $("a:contains('CrazyCreeper521')").html("<span class=\"position-manager\">Crazy_</span>")
    $("a:contains('mitchiiii')").html("<span class=\"position-developer\">mitchiii_</span>")
-   $("a:contains('Matic0B')").html("<span class=\"position-developer\">Matic0B</span>")
+   $("a:contains('Matic0B')").html("<span class=\"position-manager\">Matic0B</span>")
    $("a:contains('NerdifiedGames')").html("<span class=\"position-helper\">NerdifiedGames</span>")
    $("a:contains('xCand')").html("<span class=\"position-helper\">xCand</span>")
    $("a:contains('Jexel')").html("<span class=\"position-helper\">Bucko511</span>")
