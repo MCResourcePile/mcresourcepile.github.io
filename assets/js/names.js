12682bb050f566aa079ca8646de711f656f07a91
Matic0B <Matic0B@users.noreply.github.com>
2017-01-15 16:53:49 +0100

    Update Nanu's name

--- assets/js/names.js
+++ assets/js/names.js
@@ -7,6 +7,6 @@
    $("a:contains('Jexel')").html("<span class=\"position-helper\">Bucko511</span>")
    $("a:contains('freundTech')").html("<span class=\"position-helper\">freundTech</span>")
    $("a:contains('triplezko')").html("<span class=\"position-helper\">triplezko</span>")
-   $("a:contains('nanuscience')").html("<span class=\"position-helper\">Nanu</span>")
+   $("a:contains('NanuSci')").html("<span class=\"position-helper\">Nanu</span>")
    $("tr:contains('INT')").html("")
  };
