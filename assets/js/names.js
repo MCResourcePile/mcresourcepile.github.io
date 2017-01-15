a98c18a7320906331553d456e0f04fcb8ce72985
Matic0B <Matic0B@users.noreply.github.com>
2017-01-15 20:14:14 +0100

    Staff changes

--- assets/js/names.js
+++ assets/js/names.js
@@ -2,11 +2,11 @@
    $("a:contains('CrazyCreeper521')").html("<span class=\"position-manager\">Crazy_</span>")
    $("a:contains('mitchiiii')").html("<span class=\"position-developer\">mitchiii_</span>")
    $("a:contains('Matic0B')").html("<span class=\"position-manager\">Matic0B</span>")
-   $("a:contains('NerdifiedGames')").html("<span class=\"position-helper\">NerdifiedGames</span>")
    $("a:contains('xCand')").html("<span class=\"position-helper\">xCand</span>")
    $("a:contains('Jexel')").html("<span class=\"position-helper\">Bucko511</span>")
    $("a:contains('freundTech')").html("<span class=\"position-helper\">freundTech</span>")
    $("a:contains('triplezko')").html("<span class=\"position-helper\">triplezko</span>")
    $("a:contains('NanuSci')").html("<span class=\"position-helper\">Nanu</span>")
+   $("a:contains('nintendoboy11')").html("<span class=\"position-helper\">nintendoboy11</span>")
    $("tr:contains('INT')").html("")
  };
