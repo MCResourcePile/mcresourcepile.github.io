1a2611e1b161e71c48067f8e81f63c25e75f5195
mitchiii_ <minimitch0199@gmail.com>
2017-01-18 18:05:40 +1100

    Update Avicus map thumbnails

--- assets/materials/avicus/map-listing.js
+++ assets/materials/avicus/map-listing.js
@@ -11,8 +11,9 @@ window.onload = function() {
                 {player_name: "Matic0B", player_uuid: "48924ea3-3aad-4e16-838c-368d8d9e0a25"}
             ],
             tags: [ {dtm: true} ],
-            commercial: false,
             discussion: "",
+            official: true,
+            commercial: false,
             download: true,
             xml: true,
         }, ],
