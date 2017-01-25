230f5b7199ae349a758a231d4197a0c95895f170
Travis CI <freundtech@gmail.com>
2017-01-25 19:50:06 +0000

    Deploy to GitHub Pages: 8a9e692bece938df59f92bf51598e867093218fb

--- out/assets/materials/overcast/bow-records.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var source = document.getElementById("records-template").innerHTML;
-    var template = Handlebars.compile(source);
-    var data = {
-    };
-    var output = template(data);
-    document.getElementById("records-output").innerHTML = output;
-}
