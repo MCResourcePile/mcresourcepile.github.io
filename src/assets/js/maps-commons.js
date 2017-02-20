c552eeb33913906b1c2c0110cb10a60994904474
mitchiii_ <minimitch0199@gmail.com>
2017-02-20 17:23:08 +1100

    Move common Mapstack scripts to a maps-commons.js file

---
+++ src/assets/js/maps-commons.js
@@ -0,0 +1,96 @@
+// Common JS functions and variables which all MapStack pages use.
+// Anything that is unique to a MapStack page or can't be used by 
+// any other of the pages must be left in that document instead
+// of being put here.
+
+
+
+// Count maps with no download and send alert to console
+var total_hidden = $('.dynamic').children('.no-download').length;
+var maps_no_download = $.map($(".no-download"), function(n, i) {
+    return "\n- " + n.id;
+});
+
+if (total_hidden > 0) {
+    console.log(total_hidden + " maps have no download and have been hidden from this listing.\nPlease contact a page manager to add or enable the downloads for the following maps:" + maps_no_download)
+}
+
+
+
+// Enable thumbnail searching
+$(document).on('ready', function() {
+    $('[data-toggle="tooltip"]').tooltip()
+    makeSearchable()
+})
+
+$('.record-search-container').on('click', function() {
+    makeSearchable()
+})
+
+$(document).on('keydown', function() {
+    makeSearchable()
+});
+
+function makeSearchable() {
+    $('#maps-output .dynamic').searchable({
+        selector: 'div.map-thumbnail',
+        childSelector: '.map-float',
+        searchField: '#search'
+    })
+
+    var total_maps = $('.map-thumbnail').length
+    var total_display = $('.map-thumbnail:visible').length
+    var total_official = $('.official-map:visible').length
+    var total_unofficial = $('.unofficial-map:visible').length
+    var current_search = $('#search').val()
+
+    if (total_maps == "0") {
+        $(".total-maps").html("No maps successfully loaded<br>Please check the console for any errors and contact a page manager");
+    } else if (total_display == "0") {
+        $(".total-maps").html("Your query of <code>" + current_search + "</code> references no maps, please try searching something else");
+    } else {
+        $(".total-maps").html("Currently displaying " + total_display + " of " + total_maps + " loaded maps");
+    }
+}
+
+
+
+// Check if the map is using a license and what type it is
+// Send alerts to console before download about license
+var license = true;
+var commercial = true;
+
+function commercialLicense() {
+    license = true;
+    commercial = true;
+    console.log("This map is using a Creative Commons BY-SA 4.0 license")
+}
+
+function noncommercialLicense() {
+    license = true;
+    commercial = false;
+    console.log("This map is using a Creative Commons BY-NC-SA 4.0 license")
+}
+
+function noLicense() {
+    license = false;
+    console.log("This map has no associated license; be careful when using this map in public servers")
+}
+
+
+
+// Modal control for download progress and error messages
+GitZip.registerCallback(function(status, message, percent) {
+    if (status === "done") {
+        $("#download-complete-message").modal('show');
+        $('#download-starting-message').modal('hide');
+    } else if (status === "error") {
+        if (message.indexOf("API rate limit exceeded for") === -1) {
+            $("#download-error-message").modal('show');
+            $('#download-starting-message').modal('hide');
+        } else {
+            $("#download-rate-limit-message").modal('show');
+            $('#download-starting-message').modal('hide');
+        }
+    }
+});
