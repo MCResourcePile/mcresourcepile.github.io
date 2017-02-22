e382ffdee3d0f15791681c71359ef36fc7a78768
Dustin <dustin7212@gmail.com>
2017-02-22 16:46:45 -0600

    Remove unneeded retired staff pages

--- src/assets/js/names.js
+++ src/assets/js/names.js
@@ -4,13 +4,9 @@
    $("a:contains('mitchiiii')").html("<span class=\"position-developer\">mitchiii_</span>")
    $("a:contains('mitchiii_')").html("<span class=\"position-developer\">mitchiii_</span>")
    $("a:contains('Matic0B')").html("<span class=\"position-manager\">Matic0B</span>")
-   $("a:contains('xCand')").html("<span class=\"position-helper\">xCand</span>")
-   $("a:contains('JexeI')").html("<span class=\"position-helper\">Jexel</span>")
    $("a:contains('freundTech')").html("<span class=\"position-developer\">freundTech</span>")
    $("a:contains('triplezko')").html("<span class=\"position-helper\">triplezko</span>")
    $("a:contains('NanuSci')").html("<span class=\"position-helper\">Nanu</span>")
-   $("a:contains('nintendoboy11')").html("<span class=\"position-helper\">nintendoboy11</span>")
    $("a:contains('TheColdCrafter')").html("<span class=\"position-helper\">TheColdCrafter</span>")
-   $("a:contains('NerdifiedGames')").html("<span class=\"position-helper\">NerdifiedGames</span>")
    $("tr:contains('INT')").html("")
  };
