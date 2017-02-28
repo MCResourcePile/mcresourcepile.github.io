716b4c4d43a76105681391560c651185998488f0
Dustin <dustin7212@gmail.com>
2017-02-27 21:28:10 -0600

    Create template for League Listing pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		'compile-handlebars': {
-			info: {
-				files: [{
-					expand: true,
-					cwd: 'src/',
-					src: ['**/*.html', '!**/partials/**'],
-					dest: 'out/'
-				}],
-				helpers: 'src/helpers/title.js',
-				partials: ['src/partials/header.html', 'src/partials/footer.html', 'src/partials/alert.html', 'src/partials/map.html', 'src/partials/league.html'],
-				templateData: 'src/data/**/*.json'
-			}
-		},
-		copy: {
-			assets: {
-				expand: true,
-				cwd: 'src/assets',
-				src: '**',
-				dest: 'out/assets'
-			},
-			mapauthors: {
-				src: "src/stattrack/overcast/mapauthors.txt",
-				dest: "out/stattrack/overcast/mapauthors.txt"
-			}
-		},
-		clean: ['out/']
-	});
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'compile-handlebars': {
+            info: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.html', '!**/partials/**'],
+                    dest: 'out/'
+                }],
+                helpers: 'src/helpers/title.js',
+                partials: ['src/partials/header.html', 'src/partials/footer.html', 'src/partials/alert.html', 'src/partials/map.html', 'src/partials/league.html', 'src/partials/league-listing.html'],
+                templateData: 'src/data/**/*.json'
+            }
+        },
+        copy: {
+            assets: {
+                expand: true,
+                cwd: 'src/assets',
+                src: '**',
+                dest: 'out/assets'
+            },
+            mapauthors: {
+                src: "src/stattrack/overcast/mapauthors.txt",
+                dest: "out/stattrack/overcast/mapauthors.txt"
+            }
+        },
+        clean: ['out/']
+    });
 
-	grunt.loadNpmTasks('grunt-compile-handlebars');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-compile-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('default', ['compile-handlebars', 'copy']);
+    grunt.registerTask('default', ['compile-handlebars', 'copy']);
 }
