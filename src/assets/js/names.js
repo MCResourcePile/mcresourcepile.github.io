8fb6f6a06c02e9f00977fbd3c11be7a83847603b
mitchiiii <minimitch0199@gmail.com>
2017-06-27 16:41:32 +1000

    Update names

--- src/assets/js/names.js
+++ src/assets/js/names.js
@@ -1,12 +1,13 @@
  window.onload = function() {
    $("a:contains('CrazyCreeper521')").html("<span class=\"position-manager\">Crazy_</span>")
    $("a:contains('Crazy_')").html("<span class=\"position-manager\">Crazy_</span>")
-   $("a:contains('mitchiiii')").html("<span class=\"position-developer\">mitchiii_</span>")
-   $("a:contains('mitchiii_')").html("<span class=\"position-developer\">mitchiii_</span>")
+   $("a:contains('mitchiiii')").html("<span class=\"position-manager\">mitchiii_</span>")
+   $("a:contains('mitchiii_')").html("<span class=\"position-manager\">mitchiii_</span>")
    $("a:contains('Matic0B')").html("<span class=\"position-manager\">Matic0B</span>")
    $("a:contains('freundTech')").html("<span class=\"position-developer\">freundTech</span>")
-   $("a:contains('triplezko')").html("<span class=\"position-helper\">triplezko</span>")
+   $("a:contains('triplezko')").html("<span class=\"position-developer\">triplezko</span>")
    $("a:contains('NanuSci')").html("<span class=\"position-helper\">Nanu</span>")
-   $("a:contains('TheColdCrafter')").html("<span class=\"position-helper\">TheColdCrafter</span>")
+   $("a:contains('ViceWatercolour')").html("<span class=\"position-developer\">ViceWatercolour</span>")
+   $("a:contains('Palmidence')").html("<span class=\"position-developer\">ViceWatercolour</span>")
    $("tr:contains('INT')").html("")
  };
