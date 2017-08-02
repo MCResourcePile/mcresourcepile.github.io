2d02a48913121466dab1c7074240b112206a11e8
Travis CI <MCResourcePile@gmail.com>
2017-08-02 09:20:15 +0000

    Deploy to GitHub Pages: 15fb293e3038089e1d33c8b33d481317d6e01428

--- assets/js/commons.js
+++ assets/js/commons.js
@@ -11,5 +11,9 @@ function isAuthenticated() {
     }
 }
 
+var window_offset = function() { scrollBy(0, -70) };
+if (location.hash) window_offset();
+window.addEventListener("hashchange", window_offset);
+
 $('[data-toggle="tooltip"]').tooltip()
 $('[data-toggle="popover"]').popover()
