914ddc4bb6bcd0675135e0f111a11cf1792d71c3
mitchiiii <minimitch0199@gmail.com>
2017-08-19 00:35:49 +1000

    Trial a dark website theme

--- src/assets/js/commons.js
+++ src/assets/js/commons.js
@@ -11,6 +11,13 @@ function isAuthenticated() {
     }
 }
 
+var selected_theme = Cookies.get('rp_user_theme');
+if (selected_theme == 'dark') {
+    $('head').append('<link href="/assets/css/dark.css" rel="stylesheet">');
+} else {
+    Cookies.set('rp_user_theme', 'default', { expires: 365 });
+}
+
 var window_offset = function() { scrollBy(0, -70) };
 if (location.hash) window_offset();
 window.addEventListener("hashchange", window_offset);
