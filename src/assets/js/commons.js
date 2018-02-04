42369ac55a0584228c502982699598b427cdd32a
Mitchell <mitchiiii@users.noreply.github.com>
2018-02-04 21:22:10 +1100

    Add button to view map image when thumbnails collasped

--- src/assets/js/commons.js
+++ src/assets/js/commons.js
@@ -23,9 +23,11 @@ $('[data-toggle="tooltip"]').tooltip()
 $('[data-toggle="popover"]').popover()
 
 if (hide_map_images == 'true') {
-    $(".lazy").css({"height": "60px"}).addClass("center");
+    $(".lazy").css({"height": "60px"});
     $(".map-banner").css({"height": "60px"});
-    $(".map-labels").css({"top": "-21px"});
+    $(".map-labels").css({"top": "-21px", "padding-left": "4px"});
+    $(".map-button").addClass('map-button-sm');
+    $(".click-image").show();
 } else {
     var myLazyLoad = new LazyLoad({
         elements_selector: ".lazy",
