7754888bb77cdfef657a14942bf3d17abd5c6283
Mitchell <minimitch0199@gmail.com>
2018-04-05 16:17:32 +1000

    Update view and add alt view for hidden images

--- src/assets/js/analytics.js
+++ src/assets/js/analytics.js
@@ -4,46 +4,57 @@
  */
 
 function fetchGlobalDownloads() {
-    result = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgICAgAoM&format=jsonp', function(data){return data});
-    result.done(function() {
-        stats = result.responseJSON.rows;
-        output(stats);
+    total = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgICAgAoM&format=jsonp', function(data){return data});
+    total.done(function() {
+        stats = total.responseJSON.rows;
         for (var i = 0; i < stats.length; i++) {
-            $('.downloads-total[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
+            $('.stat-downloads-total[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
         }
     });
-    result.fail(function() {
+    total.fail(function() {
         output('Failed to retrieve "Global lifetime downloads" query from Google Analytics superProxy application.', 2);
         return;
     });
 }
 
 function fetchUniqueDownloads() {
-    result1 = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgNrjhgoM&format=jsonp', function(data){return data});
-    result1.done(function() {
-        stats = result1.responseJSON.rows;
-        output(stats);
+    unique = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgNrjhgoM&format=jsonp', function(data){return data});
+    unique.done(function() {
+        stats = unique.responseJSON.rows;
         for (var i = 0; i < stats.length; i++) {
-            $('.downloads-unique[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
+            $('.stat-downloads-unique[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
         }
     });
-    result1.fail(function() {
+    unique.fail(function() {
         output('Failed to retrieve "Global unique downloads" query from Google Analytics superProxy application.', 2);
         return;
     });
 }
 
 function fetchRecentDownloads() {
-    result2 = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgPjChAoM&format=jsonp', function(data){return data});
-    result2.done(function() {
-        stats = result2.responseJSON.rows;
-        output(stats);
+    recent = $.getJSON('https://project-id-0482060302718727966.appspot.com/query?id=aiBzfnByb2plY3QtaWQtMDQ4MjA2MDMwMjcxODcyNzk2NnIVCxIIQXBpUXVlcnkYgICAgPjChAoM&format=jsonp', function(data){return data});
+    recent.done(function() {
+        stats = recent.responseJSON.rows;
         for (var i = 0; i < stats.length; i++) {
-            $('.downloads-recent[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
+            $('.stat-downloads-recent[data-map="' + (stats[i])[0].replace(/\'/g, '-') + '"]').text((stats[i])[1]);
         }
     });
-    result2.fail(function() {
-        output('Failed to retrieve "	Global downloads (30 days)" query from Google Analytics superProxy application.', 2);
+    recent.fail(function() {
+        output('Failed to retrieve "Global downloads (30 days)" query from Google Analytics superProxy application.', 2);
         return;
     });
 }
+
+$(document).ready(function() {
+    $('.map-dropdown').click(function() {
+        open = $(this).find('.collapse').hasClass('in');
+        output(open);
+        target = $(this).find('.toggle').attr('data-toggle');
+        $('#' + target).collapse('toggle');
+        if (open) {
+            $(this).find('i').removeClass('fa-caret-up').addClass('fa-caret-down')
+        } else (
+            $(this).find('i').removeClass('fa-caret-down').addClass('fa-caret-up')
+        )
+    });
+});
