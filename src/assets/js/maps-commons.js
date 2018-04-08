c1579791bc6c0b9f4eb9983a35da3cbba5d79757
Mitchell <minimitch0199@gmail.com>
2018-04-08 21:46:36 +1000

    Direct map downloads

--- src/assets/js/maps-commons.js
+++ src/assets/js/maps-commons.js
@@ -5,61 +5,28 @@
  * of being put here.
  */
 
-var maps_hidden, maps_no_download, license_file, license_content;
-
 const is_maps_collection = true;
 
 $(document).ready(function(){
-    /* Count maps with no download and send alert to console */
+    // count maps with no download and send alert to console
     total_hidden = $('.dynamic').children('.no-download').length;
     maps_no_download = $.map($('.no-download'), function(n, i) {
         return '\n- ' + n.id;
     });
     if (total_hidden > 0) {
         output(total_hidden + ' maps have no download and have been hidden from this listing.\nPlease contact a page manager to add or enable the downloads for the following maps:' + maps_no_download)
-    }
+    };
 
-    /* Handle map download requests then send them to GitZip */
+    // handle map download requests then send them to GitZip
     $('.click-download:not(.disabled)').click(function() {
         $(this).prop('disabled', true).text('Downloading...');
         $(this).parent().find('.map-download-pending').hide();
         $(this).parent().find('.map-download-started').show();
-        var active_name = $(this).attr('id');
-        var active_slug = $(this).attr('slug');
-        var active_path = $(this).attr('path');
-        var active_license = $(this).attr('license');
-        var active_license_message;
-        if (active_license == 'none') {
-            active_license_message = 'has no associated license; be careful when using this map in public servers';
-            license_file = 'NOTICE.txt';
-            license_content = 'This work has no associated license attached to it. We recommend getting in contact with authors, which are listed on our site at https://mcresourcepile.github.io/, before using this work for purposes other than private use. Please be considerate when using this map and please respect the wishes of the authors.\n';
-        } else if (active_license == 'commercial') {
-            active_license_message = 'is using a Creative Commons BY-SA 4.0 license';
-            license_file = 'LICENSE.txt';
-            license_content = 'This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n';
-        } else if (active_license == 'noncommercial') {
-            active_license_message = 'is using a Creative Commons BY-NC-SA 4.0 license';
-            license_file = 'LICENSE.txt';
-            license_content = 'This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n';
-        }
-        output('Downloading: ' + active_slug + '\nFetching files from: ' + active_path + '\n' + active_name + ' ' + active_license_message);
-        file_name = active_slug
-        if (active_path !== undefined) {
-            if (user_settings.token) {
-                GitZip.setAccessToken(user_settings.token);
-                GitZip.zipRepo(active_path);
-                console.info('Using given GitHub API access token.');
-            } else {
-                GitZip.zipRepo(active_path);
-                console.warn('No GitHub API access token provided. Please go to your preferences to generate an access token.')
-            }
-        } else {
-            $('#download-starting-message').modal('hide');
-            $('#download-error-message').modal('show');
-            $('#download-error-output').text('No download path identified; skipping download request.');
-            progress = 0;
-            getApiLimit();
-        }
+        active_name = $(this).attr('id');
+        active_slug = $(this).attr('slug');
+        active_path = $(this).attr('path');
+        active_license = $(this).attr('license');
+        startDownload(active_name, active_slug, active_path, active_license);
     });
 
     $('.click-download-external:not(.disabled)').click(function() {
@@ -67,14 +34,60 @@ $(document).ready(function(){
         $('#download-' + active_slug).modal('hide');
         $('#download-opened').modal('show');
     });
+    
+    // handle direct downloads
+    if (getUrlVars()['dl']) {
+        var requested_map = getUrlVars()['dl'];
+        if (user_settings.automatic_downloads == 'true') {
+            model = $('#download-' + requested_map).find('.click-download');
+            active_name = $(model).attr('id');
+            active_path = $(model).attr('path');
+            active_license = $(model).attr('license');
+            startDownload(active_name, requested_map, active_path, active_license);
+        } else {
+            $('#download-' + requested_map).modal('show');
+        }
+    }
 
     updateListing();
     getApiLimit();
 
-    /* Update map listing in time with search bar interactions */
+    // update map listing in time with search bar interactions
     $('.record-search-container').click(function() { updateListing() });
     $('.record-search-container').keyup(function() { updateListing() });
 });
+
+// license definitions
+const licenses = {
+    commercial: {file: 'LICENSE.txt', contents: 'This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n', message: 'is using a Creative Commons BY-SA 4.0 license'},
+    noncommercial: {file: 'LICENSE.txt', contents: 'This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n', message: 'is using a Creative Commons BY-NC-SA 4.0 license'},
+    none: {file: 'NOTICE.txt', contents: 'This work has no associated license attached to it. We recommend getting in contact with authors, which are listed on our site at https://mcresourcepile.github.io/, before using this work for purposes other than private use. Please be considerate when using this map and please respect the wishes of the authors.\n', message: 'has no associated license; be careful when using this map in public servers'}
+};
+var imported_license;
+
+function startDownload(active_name, active_slug, active_path, active_license) {
+    if (active_license == 'commercial') imported_license = licenses.commercial;
+    if (active_license == 'noncommercial') imported_license = licenses.noncommercial;
+    if (active_license == 'none') imported_license = licenses.none;
+    output('Downloading: ' + active_slug + '\nFetching files from: ' + active_path + '\n' + active_name + ' ' + imported_license.message);
+    file_name = active_slug
+    if (active_path !== undefined) {
+        if (user_settings.token) {
+            GitZip.setAccessToken(user_settings.token);
+            GitZip.zipRepo(active_path);
+            console.info('Using given GitHub API access token.');
+        } else {
+            GitZip.zipRepo(active_path);
+            console.warn('No GitHub API access token provided. Please go to your preferences to generate an access token.')
+        }
+    } else {
+        $('#download-starting-message').modal('hide');
+        $('#download-error-message').modal('show');
+        $('#download-error-output').text('No download path identified; skipping download request.');
+        progress = 0;
+        getApiLimit();
+    }
+};
 
 function updateListing() {
     $('#maps-output .dynamic').searchable({
@@ -100,9 +113,9 @@ function updateListing() {
     } else {
         $('.total-maps').html('Currently displaying ' + total_display + ' of ' + total_maps + ' loaded maps');
     }
-}
+};
 
-/* Get GitHub API request limit information */
+// get GitHub API request limit information
 function getApiLimit() {
     updateRateLimit(function () {
         $('.api-request-remaining').text(user_info.rate.remaining).css('font-weight', 'bold');
@@ -111,9 +124,9 @@ function getApiLimit() {
         $('.api-request-reset').text(moment.unix(user_info.rate.reset).fromNow(true)).css('font-weight', 'bold');
         saveSettings();
     });
-}
+};
 
-/* Modal control for download progress and error messages */
+// modal control for download progress and error messages
 GitZip.registerCallback(function(status, message, percent) {
     var progress = percent
     if (status === 'done') {
