b58313948956099f4acf3f791ec2cf405d8130e9
Mitchell <minimitch0199@gmail.com>
2018-06-21 17:20:51 +1000

    Remove old task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jsonlint: {
-
             jshintStyle: {
                 src: [ 'src/data/**/*.json' ],
                 options: {
@@ -24,13 +23,6 @@ module.exports = function(grunt) {
                 partials: ['src/posts/*.md', 'src/partials/**/*.html'],
                 templateData: 'src/data/**/*.json',
                 globals: ['src/data/global.json']
-            },
-            downloads: {
-                files: [{
-                    src: 'src/partials/maps.js',
-                    dest: 'out/assets/js/maps.js'
-                }],
-                templateData: 'src/data/maps/overcast.json'
             }
         },
         copy: {
