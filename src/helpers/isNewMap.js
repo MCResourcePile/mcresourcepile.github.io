008cb85cf24514a90b56fadf809c2372b336ffeb
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2023-12-24 13:00:01 +1100

    End of year featured panel

--- src/helpers/isNewMap.js
+++ src/helpers/isNewMap.js
@@ -2,5 +2,6 @@ module.exports = function(created) {
     var threshold = new Date();
     threshold = threshold.setMonth(threshold.getMonth() - 2); // 2 months ago
 
-    return Date.parse(created) > threshold;
+    // return Date.parse(created) > threshold;
+    return Date.parse(created) > new Date('2023-01-01');
 };
