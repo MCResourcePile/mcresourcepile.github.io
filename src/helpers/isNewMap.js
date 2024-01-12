6ca57fea9922cdc0107e0f705092d95ec5b89f14
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2024-01-12 12:24:34 +1100

    Update isNewMap helper to automate end of year summary tagging

--- src/helpers/isNewMap.js
+++ src/helpers/isNewMap.js
@@ -1,7 +1,15 @@
-module.exports = function(created) {
-    var threshold = new Date();
-    threshold = threshold.setMonth(threshold.getMonth() - 2); // 2 months ago
+// returns true when a date is within the past 2 months
+// except in the month of december, which returns true
+// if the date is within the same year
+//
+// used to give maps a 'new' tag and highlight
 
-    // return Date.parse(created) > threshold;
-    return Date.parse(created) > new Date('2023-01-01');
+module.exports = function(created) {
+  const today = new Date();
+  var threshold = new Date().setMonth(today.getMonth() - 2);
+  if (today.getMonth() === 11) {
+    threshold = new Date(`${today.getFullYear()}-01-01`);
+  };
+
+  return Date.parse(created) > threshold;
 };
